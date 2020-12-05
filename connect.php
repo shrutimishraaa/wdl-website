4641031c9acf858feb344135d4a98256968d48b8
Shivanicloud <70951511+Shivanicloud@users.noreply.github.com>
2020-12-05 10:48:09 +0530

    Add files via upload

---
+++ connect.php
@@ -0,0 +1,41 @@
+<?php
+$username=filter_input(INPUT_POST,'username');
+$password=filter_input(INPUT_POST,'password');
+if(!empty($username)){
+if(!empty($password)){	
+$host= "localhost";
+$dbusername= "root";
+$dbpassword= "";
+$dbname= "login1";
+
+//create connection
+$conn = new mysqli($host,$dbusername,$dbpassword,$dbname);
+
+
+if(mysqli_connect_error()){
+	die('Connect Error('.mysqli_connect_errno().') ' .
+	mysqli_connect_error());
+}
+else{
+	$sql = "INSERT INTO users1(username,password)
+	values ('$username' , '$password')";
+	if($conn->query($sql)){
+		echo "NEW record is inserted sucessfully";
+	}
+	else{
+		echo "Error: ". $sql . "<br>" . $conn->error;
+	}
+	$conn->close();
+}
+}
+else{
+	echo "username should not be empty";
+	die();
+}
+}
+else{
+	echo "username should not be empty";
+	die();
+}
+
+?>
