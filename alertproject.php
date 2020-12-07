41bf2529d5ef10409fabcc05a9fa760985ee3d43
Shivanicloud <70951511+Shivanicloud@users.noreply.github.com>
2020-12-07 20:45:35 +0530

    Add files via upload

---
+++ alertproject.php
@@ -0,0 +1,105 @@
+<!doctype php>
+<php>
+<head>
+   <meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width,initial-scale=1.0">
+	<title> HOME,VIVEKANAND EDUCATION SOCIETY'S </title>
+   <link rel="stylesheet" href="style.css">
+   <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@300;400;500;600;700&display=swap" rel="stylesheet">
+   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    
+     </head>
+<body>
+<header>
+<div class="container">
+<div class="abc">
+<img src="images/logo.png" width="80px"  height="103"  alt="Institute of Technology Logo">
+</div>
+                  <h1 class="head">VIVEKANAND EDUCATION SOCIETY</br>
+			      <em>Institute of Technology</em></h1>
+				  </div>
+</header>
+   <div class="container">
+	<nav>
+     <ul>
+     <li><a href="ABOUT US.php">ABOUT US</a></li>
+     <li><a href="FACULTY.php">FACULTY</a></li>
+     <li><a href="DEPARTMENTsaloni.php">DEPARTMENTS</a></li>
+     <li><a href="GALLERY.php">GALLERY</a></li>
+      <li><a href="index.php">Attendance</a></li>
+
+     <li><a href="CONTACT US.php">CONTACT US</a></li>
+  <button type="button" class="button_1"><a href="loginsaloni.php">LOGIN</a></button>
+  </ul>
+</nav>
+</div>
+<div class="sidebar_1" style="height:150vh;">
+<div class="container">
+<ul>
+<li><a href="#">HOD's Desk</a></li>
+<li><a href="syllabus etrx.php">syllabus</a></li>
+<li><a href="DABetrx.php">DAB/PAC</a></li>
+<li><a href="labetrx.php">LAB facilities</a></li>
+<li><a href="newsletteretrx.php">newsletters</a></li>
+<li><a href="alertproject.php">projects</a></li>
+<li><a href="alertstudentachievement.php">student achievement</a></li>
+
+</ul>
+</div>
+</div>
+
+<div class="container" style="height:170vh;">
+<div class="123" style="margin-left:220px;">
+    
+         <h2 class="shivani">
+                <i class="fa fa-angle-double-right"></i>  Department of Electronics Engineering </h2>
+            <hr>
+			   <h2 class="font-weight-bold h3">Projects</h2>
+    <div class="alert alert-danger" style="margin-left:10px;background:#f9d6d5;padding:0.75rem 1.25rem;">
+        NO RECORDS FOUND
+</div>
+			   
+				  </div>
+</div>
+<!----------------FOOTER----------->
+<div class="footer">
+	<div class="container">
+		<div class="row">
+			<div class="footer-col-1">
+				<h3>Address:-</h3>
+					<p>Hashu Advani Memorial Complex,<br>
+					Collector's Colony<br>
+                    Chembur, Mumbai, Maharashtra 400074</p>	   
+					</div>
+			<div class="footer-col-2">
+				<h3>Useful Links</h3>
+					<a href="">Coupons</a>
+					<a href="">Blog Post</a>
+					<a href="">Return Policy</a>
+					<a href="">Join Affiliate</a>
+            </div>
+			<div class="footer-col-3">
+				<h3>Useful Links</h3>
+					<a href="">Coupons</a>
+					<a href="">Blog Post</a>
+					<a href="">Return Policy</a>
+					<a href="">Join Affiliate</a>
+			</div>
+			<div class="footer-col-4">
+				<h3>Follow us</h3>
+				<a href="">facebook</a>
+					<a href="">twitter</a>
+					<a href="">instagram</a>
+					<a href="">mail</a>
+			</div>
+		</div>
+		<hr>
+		<p class="copyright">Copyright 2020 - VESIT</p>
+	</div>
+</div>
+
+
+</body>
+</php>
+
+			
