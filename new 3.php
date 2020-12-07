c50d948db772ed83283f6ccb25c50e254912f94e
Shivanicloud <70951511+Shivanicloud@users.noreply.github.com>
2020-12-07 20:43:56 +0530

    Add files via upload

---
+++ new 3.php
@@ -0,0 +1,136 @@
+<!doctype html>
+<html>
+<head>
+   <meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width,initial-scale=1.0">
+	<title> HOME,VIVEKANAND EDUCATION SOCIETY'S </title>
+   <link rel="stylesheet" href="style.css">
+   <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@300;400;500;600;700&display=swap" rel="stylesheet">
+   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    
+     </head>
+<body>
+<header>
+<div class="container">
+<div class="abc">
+<img src="images/logo.png" width="80px"  height="103"  alt="Institute of Technology Logo">
+</div>
+                  <h1 class="head">VIVEKANAND EDUCATION SOCIETY</br>
+			      <em>Institute of Technology</em></h1>
+				  </div>
+</header>
+   <div class="container">
+	<nav>
+     <ul>
+     <li><a href="ABOUT US.php">ABOUT US</a></li>
+     <li><a href="FACULTY.php">FACULTY</a></li>
+     <li><a href="DEPARTMENTsaloni.php">DEPARTMENTS</a></li>
+     <li><a href="GALLERY.php">GALLERY</a></li>
+      <li><a href="index.php">Attendance</a></li>
+
+     <li><a href="CONTACT US.php">CONTACT US</a></li>
+  <button type="button" class="button_1"><a href="loginsaloni.php">LOGIN</a></button>
+  </ul>
+</nav>
+</div>
+<div class="sidebar" style="height:180%;">
+<div class="container">
+<ul>
+<li><a href="">HOD's Desk</a></li>
+<li><a href="">Syllabus</a></li>
+<li><a href="">DAB/PAC</a></li>
+<li><a href="">LAB Facilities</a></li>
+<li><a href="">Newsletters</a></li>
+<li><a href="">Projects</a></li>
+<li><a href="">Class Timetable</a></li>
+<li><a href="">Student Achievement</a></li>
+</ul>
+</div>
+</div>
+<div class="container">
+<div class="sidebar1" style="height:120vh;">
+<div class="box">
+<div class="box-img">
+<img src="images/Vaidya.jpg" alt="">
+</div>
+<div class="box-content" style="height:100vh;">
+<h3>Dr. Prakash Pandurang Vaidya</h3>
+<p class="font-weight-bold" style="font-size:15px","margin-top:10px">head of instrumentation department</p><br>
+ <p>Experienced Head instument Department with a demonstrated history of working in the instrumentation industry.</p>
+</div>
+</div>
+</div>
+</div>
+<div class="content">
+<div class="container">
+<div class="123">
+    
+         <h2 class="shivani">
+                <i class="fa fa-angle-double-right"></i> Department of
+                 
+                    Instrumentation Engineering
+                            </h2>
+            <hr>
+               <div class="col-md-8 col-sm-8 m-0 bg-light text-justify">
+			   <h2 class="font-weight-bold h3">HoD's Desk</h2>
+			   <div class="11">
+                    <p align="justify">Field of Instrumentation is rapidly expanding its boundaries because of demanding applications. These include applications in field of process instrumentation, nuclear instrumentation, bio-medocal instrumentation and fiber optic instrumentation to mention a few. . The scope of instrumentation has been further advanced by recent developments in filed of MEMS and nanotechnology. This has led to use of advanced technology for instrumentation .Use of smart transducers and embedded systems has become commonplace. Many designs make use of intelligent and autonomous systems for control applications. Low power compact design for portable instrumentation using semiconductors is becoming popular. </p><p align="justify">It is a challenging job to educate and produce instrumentation engineer who will be capable and confident of developing and handling such advanced instruments. Instrumentation dept of VESIT is accomplishing job of creating such instrumentation engineers who will meet requirements at national as well as international level. In addition to graduate course leading to degree of B.E.(Instrumentation)from Mumbai University, the department also conducts P.G. course for M.E.(Instrumentation &amp; Controls).The department has well qualified, experienced &amp; highly dedicated staff and advanced labs including process instrumentation lab ,control system lab, Analytical &amp; transducer lab ,DSP lab ad Biomedical Lab.The department has recently set up a well equipped automation lab. in collaboration with AIA. The department also has close interaction with R&amp;D centers and industries for practical applications.</p>
+                    <p><strong>Vision:</strong></p>
+                       <ol>
+					   <li>To contribute at National as well as at International level for excellence in technical education and research in field of Instrumentation &amp; Controls by creating proactive, noble personalities and integrated individuals who will use the knowledge and be fully dedicated to the progress of society.</li>
+</ol>
+<p><strong>Mission:</strong></p>
+<ol>
+<li>To provide superior education to students to develop critical, analytical and imaginative thinking to enter the workplace with confidence.</li>
+<li>To create an environment for promoting new ideas and research to generate innovators and future leaders.</li>
+<li>To promote academic growth by offering state of art education at undergraduate, post graduate and doctoral level and to pursue lifelong learning so as to lead towards the Field of Instrumentation is a rapidly expanding its boundaries because of demanding applications. These include applications in field of nuclear instrumentation, accelerator instrumentation, instrumentation for high energy physics and astrophysics experiments in addition to process control and bio-medocal instrumentation. The scope of instrumentation has been further advanced by recent developments in field of MEMS and nanotechnology. This has led to use of advanced technology for instrumentation .Use of smart transducers and embedded systems has become commonplace.</li>
+</ol>
+<br>
+<p>Many designs make use of intelligent and autonomous systems for control applications. Low power compact design for portable instrumentation using semiconductors is becoming popular. It is a challenging job to educate and produce instrumentation engineer who will be capable and confident of developing and handling such advanced instruments. Instrumentation dept of VESIT is accomplishing job of creating such instrumentation engineers who will meet requirements at national as well as international level.<br>
+In addition to graduate course leading to degree of B.E.(Instrumentation)from Mumbai University, the department also conducts P.G. course for M.E.(Instrumentation &amp; Controls).The department has well qualified, experienced &amp; highly dedicated staff and advanced labs including process instrumentation lab ,control system lab, Analytical &amp; transducer lab ,DSP lab ad Biomedical Lab. The department also has close interaction with R&amp;D centres and industries for practical applications.</p>
+
+                    </div>
+                </div>  
+				  </div>
+</div>
+</div>
+<!----------------FOOTER----------->
+<div class="footer">
+	<div class="container">
+		<div class="row">
+			<div class="footer-col-1">
+				<h3>Address:-</h3>
+					<p>Hashu Advani Memorial Complex,<br>
+					Collector's Colony<br>
+                    Chembur, Mumbai, Maharashtra 400074</p>	   
+					</div>
+			<div class="footer-col-2">
+				<h3>Useful Links</h3>
+					<a href="">Coupons</a>
+					<a href="">Blog Post</a>
+					<a href="">Return Policy</a>
+					<a href="">Join Affiliate</a>
+            </div>
+			<div class="footer-col-3">
+				<h3>Useful Links</h3>
+					<a href="">Coupons</a>
+					<a href="">Blog Post</a>
+					<a href="">Return Policy</a>
+					<a href="">Join Affiliate</a>
+			</div>
+			<div class="footer-col-4">
+				<h3>Follow us</h3>
+				<a href="">facebook</a>
+					<a href="">twitter</a>
+					<a href="">instagram</a>
+					<a href="">mail</a>
+			</div>
+		</div>
+		<hr>
+		<p class="copyright">Copyright 2020 - VESIT</p>
+	</div>
+</div>
+
+
+</body>
+</html>
